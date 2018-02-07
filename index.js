59bbb48bb0c4fa8478723837ffccfc4f0719656e
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 14:19:59 +0700

    REVISI CREATE NESTED

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function nestedArray(jumlahRow,jumlahCol){
   var chars = "abcdefghijklmnopqrstuvwxyz";
   var arrAlphabet=[];
   for(var i=0;i<jumlahRow;i++) {
-      arrAlphabet.push([]);
-      for(var j=0;j<jumlahCol;j++) {
+    arrAlphabet.push([]);
+    for(var j=0;j<jumlahCol;j++) {
       arrAlphabet[i].push(chars[Math.floor(Math.random() * chars.length)]);
-      }
+    }
   }
   return arrAlphabet;
 }
