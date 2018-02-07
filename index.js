5bec97caedb5f48f2a07c89c3febbf22ba139b33
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 13:00:50 +0700

    CREATED NESTED ARRAY

---
+++ index.js
@@ -0,0 +1,13 @@
+function nestedArray(jumlahRow,jumlahCol){
+  var chars = "abcdefghijklmnopqrstuvwxyz";
+  var arrAlphabet=[];
+  for(var i=0;i<jumlahRow;i++) {
+      arrAlphabet.push([]);
+      for(var j=0;j<jumlahCol;j++) {
+      arrAlphabet[i].push(chars[Math.floor(Math.random() * chars.length)]);
+      }
+  }
+  return arrAlphabet;
+}
+
+console.log(nestedArray(4,3));
