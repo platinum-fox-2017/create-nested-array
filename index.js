32efaeb553113b6bae55089839ba0962f45191f1
srohimah <sitirohimach@gmail.com>
2018-02-07 16:07:19 +0700

    implementasi dengan class

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log('---using function---')
 const createNestedArr = (jumlaRow, jumlahCol)=>{
     let nestedArr=[]
     let alpha ='abcdefghijklmnopqrstuvwxyz'
@@ -14,3 +15,31 @@ const createNestedArr = (jumlaRow, jumlahCol)=>{
 
 console.log(createNestedArr(5,3))
 console.log(createNestedArr(4,2))
+
+
+console.log('\n---using class---')
+class CreateNestedArr {
+    constructor(row, col){
+        this.row = row
+        this.col = col
+        this.alpha = 'abcdefghijklmnopqrstuvwxyz'
+    }
+    randomAlpha(){
+        let random = Math.floor(Math.random()*26)
+        return this.alpha[random]
+    }
+    printBoard(){
+        let nestedArr=[]
+        for(let i = 0; i<this.row; i++){
+            let arr =[]
+            for(let j=0; j<this.col; j++){
+                arr.push(this.randomAlpha())
+            }
+            nestedArr.push(arr)
+        }
+        return nestedArr       
+    }
+}
+
+let nestedArr = new CreateNestedArr(5,4)
+console.log(nestedArr.printBoard())
