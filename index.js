79af9df8d2550142d4b14515f50a13a516c11cd8
marcosumali <marco.sumali90@gmail.com>
2018-02-08 11:16:33 +0700

    benerin indentasi

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function createNestedArr(jumlahRow, jumlahCol) {
     
     for (var i = 0; i < jumlahRow; i++) {
         kursi.push([]);
-            for (var j = 0; j < jumlahCol; j++) {
-                var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-                // console.log(alphabet.length); // 26
-                var angkaRandom = Math.round(Math.random()*26); 
+        for (var j = 0; j < jumlahCol; j++) {
+            var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+            // console.log(alphabet.length); // 26
+            var angkaRandom = Math.round(Math.random()*26); 
             kursi[i].push(alphabet[angkaRandom]);
         }
     }
